6be6d235b3ecf193389964a83e3a79c3eb78a62f
Martin Sidery <mjs10@doc.ic.ac.uk>
2012-07-18 16:38:25 +0100

    simpler setup of button bindings. no need to go through xml again

--- assets/www/ui.js
+++ assets/www/ui.js
@@ -21,8 +21,8 @@ function createTabs() {
 
 	var yhtml = new Array();
 	var x = xmlDoc.getElementsByTagName("LEVEL0");
-	var xhtml = '<div id="toplevel" style="width:'+width+'">\n';
-	xhtml += '<div id="control" data-role="controlgroup" data-type="horizontal">\n';
+	var xhtml = '<div id="toplevel" class="sub" style="width:'+width+'">\n';
+	xhtml += '<div id="control" class="controlgroup" data-role="controlgroup" data-type="horizontal">\n';
 	for (var i = 0; i < x.length; i++) {
 		xhtml += '<a id="btn'+i+'" class="opt" data-role="button" data-theme="a"';
 		if (i != 0 && i%4 == 0)
@@ -32,8 +32,8 @@ function createTabs() {
 		xhtml += '>' + x[i].childNodes[0].nodeValue + '</a>\n';
 		var zhtml = new Array();
 		var y = x[i].getElementsByTagName("LEVEL1");
-		yhtml[i] = '<div id="subtab'+i+'" class="subtab" >\n';
-		yhtml[i] += '<div id="control'+i+'" class="control" data-role="controlgroup" data-type="horizontal">\n';
+		yhtml[i] = '<div id="subtab'+i+'" class="subtab sub" >\n';
+		yhtml[i] += '<div id="control'+i+'" class="control controlgroup" data-role="controlgroup" data-type="horizontal">\n';
 		for (var j = 0; j < y.length; j++) {
 			yhtml[i] += '<a id="btn'+i+'-'+j+'" class="opt" data-role="button" data-theme="a"';
 			if (j != 0 && j%4 == 0)
@@ -42,8 +42,8 @@ function createTabs() {
 				yhtml[i] += ' data-icon="arrow-r" data-iconpos="right"';
 			yhtml[i] += '>' + y[j].childNodes[0].nodeValue + '</a>\n';
 			
-			zhtml[j] = '<div id="subtab'+i+'-'+j+'" class="subtab'+i+'" >\n';
-			zhtml[j] += '<div id="control'+i+'-'+j+'" class="control'+i+'" data-role="controlgroup" data-type="horizontal">\n';
+			zhtml[j] = '<div id="subtab'+i+'-'+j+'" class="subtab'+i+' sub" >\n';
+			zhtml[j] += '<div id="control'+i+'-'+j+'" class="control'+i+' controlgroup" data-role="controlgroup" data-type="horizontal">\n';
 			var z = y[j].getElementsByTagName("LEVEL2");
 			for (var k = 0; k < z.length; k++) {
 				zhtml[j] += '<a id="btn'+i+'-'+j+'-'+k+'" class="opt" data-role="button" data-theme="a"';
@@ -68,70 +68,43 @@ function createTabs() {
 }
 
 function addBindings(level) {
-	function apply(level) {
-		$('#control'+level+' .opt').bind('click', function() {
-			var num = $(this).attr('id').substring(3);
+	$('.controlgroup .opt').bind('click', function() {
+		var num = $(this).attr('id').substring(3);
+		var lev = $(this).parent().attr('id').substring(7);
+		if (lev.length < 3) {
 			// $('#forms').css('display', 'none');
-			$('#control'+level+' .opt').removeClass('ui-btn-down-b');
+			$('#control'+lev+' .opt').removeClass('ui-btn-down-b');
 			$(this).addClass('ui-btn-down-b');
 	
-			$('.subtab'+level).css('display', 'none');
+			$('.subtab'+lev).css('display', 'none');
 			$('#subtab'+num).css('display', 'block');
-		});
-	}
-	function extras(level) {
-		$('#control'+level+' .opt').bind('click', function() {
-			var num = $(this).attr('id').substring(3);
-			$('#text').text(document.getElementById('text').value+';'+$(this).text());
-		});
-	}
-	function addSwipe(level) {
-		$('.control'+level).bind('swipeleft', function() {
-			$(this).animate({ marginLeft: -width }, slideSpeed, function () {});
-		});
-		
-		$('.control'+level).bind('swiperight', function() {
-			$(this).animate({ marginLeft: 0 }, slideSpeed, function () {});
-		});
-	}
-	function setupSlide(level) {
-		
-	}
-	
-	var xmlhttp;
-	if (window.XMLHttpRequest)
-		xmlhttp = new XMLHttpRequest();
-	else
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	xmlhttp.open("GET", "tabs.xml", false);
-	xmlhttp.send();
-	var xmlDoc = xmlhttp.responseXML;
-	
-	$('.subtab').css('display', 'none');
-	apply('');
-	addSwipe('');
-	var x = xmlDoc.getElementsByTagName("LEVEL0");
-	for (var i = 0; i < x.length; i++) {
-		$('.subtab'+i).css('display', 'none');
-		apply(i);
-		addSwipe(i);
-		var y = x[i].getElementsByTagName("LEVEL1");
-		for (var j = 0; j < y.length; j++) {
-			extras(i+'-'+j);
-			addSwipe(i+'-'+j);
+			resetEntries();
 		}
-	}
+		else {
+			$(this).addClass('ui-btn-down-b');
+			$('#text').text(document.getElementById('text').value+';'+$(this).text());
+		}
+	});
+	$('.controlgroup').bind('swipeleft', function() {
+		$(this).animate({ marginLeft: -width }, slideSpeed, function () {});
+	});
+	$('.controlgroup').bind('swiperight', function() {
+		$(this).animate({ marginLeft: 0 }, slideSpeed, function () {});
+	});
 	
 	$('#enter').bind('click', function() {
 		// add stuff to the DB
-		resetHighlights();
-		$('#text').text('');
+		resetEntries();
 	});
 	
 	$('#cancel').bind('click', function() {
-		resetHighlights();
-		$('#text').text('');
+		resetEntries();
 	});
+	
+	$('.sub').css('display', 'none');
+	$('#toplevel').css('display', 'block');
+	$('#subtab0').css('display', 'block');
+	$('#subtab0-0').css('display', 'block');
 }
 
 function initHighlights() {
@@ -143,8 +116,9 @@ function initHighlights() {
 	$('#subtab1-0').css('display', 'block');
 }
 
-function resetHighlights() {
-	
+function resetEntries() {
+	$('.sub .sub .sub .opt').removeClass('ui-btn-down-b');
+	$('#text').text('');
 }
 
 function setupDimensions() {
