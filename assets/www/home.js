627c69c4c21999bee88973e5c983432a45c8742b
Martin Sidery <mjs10@doc.ic.ac.uk>
2012-07-20 13:00:12 +0100

    forgot to add home.js

---
+++ assets/www/home.js
@@ -0,0 +1,41 @@
+function showHomeUI() {
+	$('.wrapper').css('display', 'none');
+	$('#homewrapper').css('display', 'block');
+	$('input').val('');
+}
+
+function initHomeUI () {
+	setupHomeUI();
+}
+
+function setupHomeUI () {
+	var form = '<form action="javascript:gotoCommentUI()" >';
+	form += '<input id="fname" type="text" placeholder="First name" />';
+	form += '<input id="lname" type="text" placeholder="Surname" />';
+	form += '<input id="subject" type="text" placeholder="Subject" />';
+	form += '<input id="module" type="text" placeholder="Module" />';
+	form += '<input id="start" type="submit" data-inline="true" value="Start Session" data-role="button" data-theme="c" >';
+	form += '</form>';
+	$('#homewrapper').html(form).trigger('create');
+}
+
+function gotoCommentUI() {
+	selectData('SELECT * FROM session ORDER BY sessionID DESC',
+			function(tx, results) {
+				var data = new Array();
+				
+				if (results.rows.length == 0)
+					data[0] = 0;
+				else
+					data[0] = results.rows.item(0).sessionID + 1;
+				
+				data[1] = getDate();
+				data[2] = document.getElementById('fname').value;
+				data[3] = document.getElementById('lname').value;
+				data[4] = document.getElementById('subject').value;
+				data[5] = document.getElementById('module').value;
+				showCommentUI(data);
+			},
+			'Failed to get a session ID!'
+	);
+}
