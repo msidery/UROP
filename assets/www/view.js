b71b98edcaef8c42f40ff4b04a16216b32452149
Martin Sidery <mjs10@doc.ic.ac.uk>
2012-07-23 12:09:26 +0100

    delete and back buttons now grouped in view mode

--- assets/www/view.js
+++ assets/www/view.js
@@ -1,7 +1,9 @@
 function initViewUI() {
 	var body = '<img id="picture" style="float:center;"></img>';
+	body += '<div data-role="controlgroup" data-type="horizontal" >'
 	body += '<a id="delete" data-role="button" data-theme="a" >Delete</a>';
 	body += '<a id="viewback" data-role="button" data-theme="a" >Back</a>';
+	body += '</div>';
 	$('#viewwrapper').html(body).trigger('create');
 	
 	$('#delete').bind('click', function() {
@@ -12,8 +14,10 @@ function initViewUI() {
 		showCommentUI();
 	});
 	
-	$('#picture').css('height', height-2*($('#delete').height()+10)+'px');
+	$('#picture').css('height', height-$('#delete').height()-3+'px');
 	$('#picture').css('width', width+'px');
+	$('#delete').css('width', width/2-1+'px');
+	$('#viewback').css('width', width/2-1+'px');
 }
 
 function showViewUI(url) {
